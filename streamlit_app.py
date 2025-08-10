7e87df8776f754870d597970675094a201ff4a39
DavidDiasData <daviddiasrodriguez@gmail.com>
2025-08-10 22:05:53 +0000

    Create the upload file feature

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ st.set_page_config(page_title='Bayesian AB Testing Calculator',
 st.title('📊 Bayesian AB Testing Calculator')
 st.caption('Made by :blue[Sterling]')
 
-st.caption('🚀 :green[New feature:] Download your results in a csv file')
-st.caption('💪🏾 :blue[Work in progress:] Upload your files / Connect your Google Sheets and BigQuery accounts')
+st.caption('🚀 :green[New feature:] Upload your CSV files')
+st.caption('💪🏾 :blue[Work in progress:] Connect your Google Sheets and BigQuery accounts')
 
 def getAlphaBeta(mu, sigma):
     alpha = mu**2 * ((1 - mu) / sigma**2 - 1 / mu)
@@ -57,11 +57,13 @@ with st.expander('Settings'):
  'beta': beta_prior_results['beta']}
   table_beta = pd.DataFrame(data=beta_data, index=[0])
   st.table(table_beta)
-  #st.warning('To engage with the app: <br />1. Find your optimal beta priors <br /> 2. Select number of variants you want to analyze. <br /> 3. Add your values and voilá.')
   variant_number = st.slider("How many variants you will analyze?", 2, 4, 2)
-  manual_input, upload_file, google_connector = st.tabs(["Manual", "Upload file", "Google Sheets /BigQuery Connection"])
+  type_input_data = st.selectbox('Select your data source', ['Manual','Upload file','Google Sheets / BigQuery Connection'])
+  st.divider()  # 👈 Draws a horizontal rule
+  
+  #manual_input, upload_file, google_connector = st.tabs(["Manual", "Upload file", "Google Sheets /BigQuery Connection"])
 
-  with manual_input:
+  if type_input_data == 'Manual':
     control_users = st.number_input("Control users", value=1000, placeholder="Type the control users here", min_value=0)
     control_purchases = st.number_input("Control interactions", value=50, placeholder="Type the control purchases here", min_value=0)
     v1_users = st.number_input("V1 users", value=1000, placeholder="Type a V1 users here", min_value=0)
@@ -89,155 +91,216 @@ with st.expander('Settings'):
                   [v2_users,v2_purchases],
                   [v3_users,v3_purchases]]
       
-  with upload_file:
-    st.markdown('**@DavidDias is working on it**')
-  with google_connector:
-    st.markdown('**@DavidDias is working on it**')
+  if type_input_data == 'Upload file':
+    uploaded_file = st.file_uploader("Upload a file (CSV files only*)")
 
-beta_simulations = {}
+    pass  # Initialize values_list as an empty list
 
-control_beta_values = np.random.beta(values_list[0][1] + beta_prior_results['alpha'], 
-                                 values_list[0][0] - values_list[0][1] + beta_prior_results['beta'], 10000)
+    if uploaded_file is None:
+        pass
+    if uploaded_file is not None:
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
 
-box_plot_results = pd.DataFrame(columns=['variant', 'CR',  'diff'], index=None)
+        list_descriptions = ['Select the control users', 
+                             'Select the control conversions',
+                             'Select the v1 users',
+                             'Select the v1 conversions',
+                             'Select the v2 users',
+                             'Select the v2 conversions',
+                             'Select the v3 users',
+                             'Select the v3 conversions']
 
-control_beta_values = np.random.beta(values_list[0][1] + beta_prior_results['alpha'], 
-                                 values_list[0][0] - values_list[0][1] + beta_prior_results['beta'], 10000)
+        dataframe
+        if len(list(dataframe.columns)) >= 4 and variant_number == 2:
+            variant_name = ['control', 'v1']
+            control_users = int(dataframe[list(dataframe.columns)[0]][0])
+            control_purchases = int(dataframe[list(dataframe.columns)[1]][0])
+            v1_users = int(dataframe[list(dataframe.columns)[2]][0])
+            v1_purchases = int(dataframe[list(dataframe.columns)[3]][0])
+            values_list = [[control_users, control_purchases],
+                           [v1_users, v1_purchases]]
 
-new_df = pd.DataFrame(data={'variant': variant_name[0], 'CR': control_beta_values, 'diff':control_beta_values*0})
-box_plot_results = pd.concat([box_plot_results, new_df], ignore_index=True)
-beta_simulations.update({variant_name[0]: control_beta_values})
+        elif len(list(dataframe.columns)) >= 6 and variant_number == 3:
+            variant_name = ['control', 'v1', 'v2']
+            control_users = int(dataframe[list(dataframe.columns)[0]][0])
+            control_purchases = int(dataframe[list(dataframe.columns)[1]][0])
+            v1_users = int(dataframe[list(dataframe.columns)[2]][0])
+            v1_purchases = int(dataframe[list(dataframe.columns)[3]][0])
+            v2_users = int(dataframe[list(dataframe.columns)[4]][0])
+            v2_purchases = int(dataframe[list(dataframe.columns)[5]][0])
+            values_list = [[control_users,control_purchases],
+                  [v1_users,v1_purchases],
+                  [v2_users,v2_purchases]]
 
-interval_values = {}
-interval_values.update({variant_name[0]: [round(np.percentile(box_plot_results['CR'], 10), 4), round(np.percentile(box_plot_results['CR'], 90), 4)]})
-
-for i in range(len(values_list[0:])):
+        elif len(list(dataframe.columns)) >= 8 and variant_number == 4:
+            variant_name = ['control', 'v1', 'v2', 'v3']
+            control_users = int(dataframe[list(dataframe.columns)[0]][0])
+            control_purchases = int(dataframe[list(dataframe.columns)[1]][0])
+            v1_users = int(dataframe[list(dataframe.columns)[2]][0])
+            v1_purchases = int(dataframe[list(dataframe.columns)[3]][0])
+            v2_users = int(dataframe[list(dataframe.columns)[4]][0])
+            v2_purchases = int(dataframe[list(dataframe.columns)[5]][0])
+            v3_users = int(dataframe[list(dataframe.columns)[6]][0])
+            v3_purchases = int(dataframe[list(dataframe.columns)[7]][0])
+            values_list = [[control_users,control_purchases],
+                    [v1_users,v1_purchases],
+                    [v2_users,v2_purchases],
+                    [v3_users,v3_purchases]]
     
-    beta_values = np.random.beta(values_list[i][1] + beta_prior_results['alpha'], 
-                                 values_list[i][0] - values_list[i][1] + beta_prior_results['beta'], 10000)
-    beta_simulations.update({variant_name[i]: beta_values})
-    new_df = pd.DataFrame(data={'variant': variant_name[i], 'CR': beta_values, 'diff':beta_values - control_beta_values})
-    box_plot_results = pd.concat([box_plot_results, new_df], ignore_index=True)
-    interval_values.update({variant_name[i]: [round(np.percentile(new_df['CR'], 10), 4), round(np.percentile(new_df['CR'], 90), 4)]})
-
-
-
-tab0, tab1, tab2 = st.tabs(["All Variants", "Difference vs Control", "Prior and Posterior Data"])
-
-
-
-  
-with tab0:
-  
-  boxplot = px.box(box_plot_results, x="variant", y="CR", color="variant")
-  st.plotly_chart(boxplot, use_container_width=True)
-
-with tab1:
-  
-  box_plot_results_wo_control = box_plot_results.loc[box_plot_results['variant'] != 'control']
-  boxplot_diff = px.box(box_plot_results_wo_control, x="variant", y="diff", color="variant")
-  st.plotly_chart(boxplot_diff, use_container_width=True)
-
-with tab2:
-  prior_control_beta_values = np.random.beta(beta_prior_results['alpha'], beta_prior_results['beta'], 10000)
-  df_prior_posterior = pd.DataFrame({'prior': prior_control_beta_values,
-                       'posterior': control_beta_values})
-  fig = go.Figure()
-  fig.add_trace(go.Histogram(x=prior_control_beta_values, name='prior'))
-  fig.add_trace(go.Histogram(x=control_beta_values, name='posterior'))
-  fig.update_layout(barmode='overlay')
+  if type_input_data == 'Google Sheets / BigQuery Connection':
+      st.markdown('**@DavidDias is working on it**')
+      pass
+      
     
-  fig.update_traces(opacity=0.75)
-  st.plotly_chart(fig, theme="streamlit")
-  table_beta = pd.DataFrame(data=beta_data, index=[0])
 
-st.subheader('Results:')
+   
+if 'values_list' in globals():
 
-diff_v1 = beta_simulations['v1'] - beta_simulations['control']
-loss_v1 = diff_v1[diff_v1<0]
-loss_list = [((diff_v1 > 0).mean())*100]
+  beta_simulations = {}
 
-csv = pd.DataFrame(data={'Experiment Name': 'Undefined',
-                    'control_users': control_users,
-                     'control_conversions': control_purchases,
-                      'v1_users': v1_users,
-                       'v1_conversions': v1_purchases,
-                          'control_percentil_10_cr': interval_values['control'][0],
-                          'control_percentil_90_cr': interval_values['control'][1], 
-                          'v1_percentil_10_cr': interval_values['v1'][0],
-                          'v1_percentil_90_cr': interval_values['v1'][1],
-                           'v1_prob_to_win_control': loss_list[0],}, index=[0])
+  control_beta_values = np.random.beta(values_list[0][1] + beta_prior_results['alpha'], 
+                                  values_list[0][0] - values_list[0][1] + beta_prior_results['beta'], 10000)
+
+  box_plot_results = pd.DataFrame(columns=['variant', 'CR',  'diff'], index=None)
+
+  control_beta_values = np.random.beta(values_list[0][1] + beta_prior_results['alpha'], 
+                                  values_list[0][0] - values_list[0][1] + beta_prior_results['beta'], 10000)
+
+  new_df = pd.DataFrame(data={'variant': variant_name[0], 'CR': control_beta_values, 'diff':control_beta_values*0})
+  box_plot_results = pd.concat([box_plot_results, new_df], ignore_index=True)
+  beta_simulations.update({variant_name[0]: control_beta_values})
+
+  interval_values = {}
+  interval_values.update({variant_name[0]: [round(np.percentile(box_plot_results['CR'], 10), 4), round(np.percentile(box_plot_results['CR'], 90), 4)]})
+
+  for i in range(len(values_list[0:])):
+      beta_values = np.random.beta(values_list[i][1] + beta_prior_results['alpha'], 
+                                  values_list[i][0] - values_list[i][1] + beta_prior_results['beta'], 10000)
+      beta_simulations.update({variant_name[i]: beta_values})
+      new_df = pd.DataFrame(data={'variant': variant_name[i], 'CR': beta_values, 'diff':beta_values - control_beta_values})
+      box_plot_results = pd.concat([box_plot_results, new_df], ignore_index=True)
+      interval_values.update({variant_name[i]: [round(np.percentile(new_df['CR'], 10), 4), round(np.percentile(new_df['CR'], 90), 4)]})
 
 
-if variant_number == 3:
-  diff_v2 = beta_simulations['v2'] - beta_simulations['control']
-  loss_v2 = diff_v2[diff_v2<0]
-  loss_list = [((diff_v1 > 0).mean())*100, ((diff_v2 > 0).mean())*100]
+
+  tab0, tab1, tab2 = st.tabs(["All Variants", "Difference vs Control", "Prior and Posterior Data"])
+
+
+
+    
+  with tab0:
+    
+    boxplot = px.box(box_plot_results, x="variant", y="CR", color="variant")
+    st.plotly_chart(boxplot, use_container_width=True)
+
+  with tab1:
+    
+    box_plot_results_wo_control = box_plot_results.loc[box_plot_results['variant'] != 'control']
+    boxplot_diff = px.box(box_plot_results_wo_control, x="variant", y="diff", color="variant")
+    st.plotly_chart(boxplot_diff, use_container_width=True)
+
+  with tab2:
+    prior_control_beta_values = np.random.beta(beta_prior_results['alpha'], beta_prior_results['beta'], 10000)
+    df_prior_posterior = pd.DataFrame({'prior': prior_control_beta_values,
+                        'posterior': control_beta_values})
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(x=prior_control_beta_values, name='prior'))
+    fig.add_trace(go.Histogram(x=control_beta_values, name='posterior'))
+    fig.update_layout(barmode='overlay')
+      
+    fig.update_traces(opacity=0.75)
+    st.plotly_chart(fig, theme="streamlit")
+    table_beta = pd.DataFrame(data=beta_data, index=[0])
+
+  st.subheader('Results:')
+  diff_v1 = beta_simulations['v1'] - beta_simulations['control']
+  loss_v1 = diff_v1[diff_v1<0]
+  loss_list = [((diff_v1 > 0).mean())*100]
 
   csv = pd.DataFrame(data={'Experiment Name': 'Undefined',
-                    'control_users': control_users,
-                     'control_conversions': control_purchases,
-                      'v1_users': v1_users,
-                       'v1_conversions': v1_purchases,
-                        'v2_users': v2_users,
-                         'v2_conversions': v2_purchases,
-                          'control_percentil_10_cr': interval_values['control'][0],
-                          'control_percentil_90_cr': interval_values['control'][1], 
-                          'v1_percentil_10_cr': interval_values['v1'][0],
-                          'v1_percentil_90_cr': interval_values['v1'][1],
-                           'v2_percentil_10_cr': interval_values['v2'][0],
-                          'v2_percentil_90_cr': interval_values['v2'][1],
-                           'v1_prob_to_win_control': loss_list[0],
-                           'v2_prob_to_win_control': loss_list[1]}, index=[0])
+                      'control_users': control_users,
+                      'control_conversions': control_purchases,
+                        'v1_users': v1_users,
+                        'v1_conversions': v1_purchases,
+                            'control_percentil_10_cr': interval_values['control'][0],
+                            'control_percentil_90_cr': interval_values['control'][1], 
+                            'v1_percentil_10_cr': interval_values['v1'][0],
+                            'v1_percentil_90_cr': interval_values['v1'][1],
+                            'v1_prob_to_win_control': loss_list[0],}, index=[0])
+
+
+  if variant_number == 3:
+    diff_v2 = beta_simulations['v2'] - beta_simulations['control']
+    loss_v2 = diff_v2[diff_v2<0]
+    loss_list = [((diff_v1 > 0).mean())*100, ((diff_v2 > 0).mean())*100]
+
+    csv = pd.DataFrame(data={'Experiment Name': 'Undefined',
+                      'control_users': control_users,
+                      'control_conversions': control_purchases,
+                        'v1_users': v1_users,
+                        'v1_conversions': v1_purchases,
+                          'v2_users': v2_users,
+                          'v2_conversions': v2_purchases,
+                            'control_percentil_10_cr': interval_values['control'][0],
+                            'control_percentil_90_cr': interval_values['control'][1], 
+                            'v1_percentil_10_cr': interval_values['v1'][0],
+                            'v1_percentil_90_cr': interval_values['v1'][1],
+                            'v2_percentil_10_cr': interval_values['v2'][0],
+                            'v2_percentil_90_cr': interval_values['v2'][1],
+                            'v1_prob_to_win_control': loss_list[0],
+                            'v2_prob_to_win_control': loss_list[1]}, index=[0])
 
 
 
-if variant_number == 4:
-   diff_v2 = beta_simulations['v2'] - beta_simulations['control']
-   diff_v3 = beta_simulations['v3'] - beta_simulations['control']
-   loss_v2 = diff_v2[diff_v2<0]
-   loss_v3 = diff_v3[diff_v3<0]
-   loss_list = [((diff_v1 > 0).mean())*100, ((diff_v2 > 0).mean())*100, ((diff_v3 > 0).mean())*100]
-   csv = pd.DataFrame(data={'Experiment Name': 'Undefined',
-                    'control_users': control_users,
-                     'control_conversions': control_purchases,
-                      'v1_users': v1_users,
-                       'v1_conversions': v1_purchases,
-                        'v2_users': v2_users,
-                         'v2_conversions': v2_purchases,
-                          'v3_users': v3_users,
-                         'v3_conversions': v3_purchases,
-                          'control_percentil_10_cr': interval_values['control'][0],
-                          'control_percentil_90_cr': interval_values['control'][1], 
-                          'v1_percentil_10_cr': interval_values['v1'][0],
-                          'v1_percentil_90_cr': interval_values['v1'][1],
-                           'v2_percentil_10_cr': interval_values['v2'][0],
-                          'v2_percentil_90_cr': interval_values['v2'][1],
-                          'v3_percentil_10_cr': interval_values['v3'][0],
-                          'v3_percentil_90_cr': interval_values['v3'][1],
-                           'v1_prob_to_win_control': loss_list[0],
-                           'v2_prob_to_win_control': loss_list[1],
-                           'v3_prob_to_win_control': loss_list[2]}, index=[0])
+  if variant_number == 4:
+    diff_v2 = beta_simulations['v2'] - beta_simulations['control']
+    diff_v3 = beta_simulations['v3'] - beta_simulations['control']
+    loss_v2 = diff_v2[diff_v2<0]
+    loss_v3 = diff_v3[diff_v3<0]
+    loss_list = [((diff_v1 > 0).mean())*100, ((diff_v2 > 0).mean())*100, ((diff_v3 > 0).mean())*100]
+    csv = pd.DataFrame(data={'Experiment Name': 'Undefined',
+                      'control_users': control_users,
+                      'control_conversions': control_purchases,
+                        'v1_users': v1_users,
+                        'v1_conversions': v1_purchases,
+                          'v2_users': v2_users,
+                          'v2_conversions': v2_purchases,
+                            'v3_users': v3_users,
+                          'v3_conversions': v3_purchases,
+                            'control_percentil_10_cr': interval_values['control'][0],
+                            'control_percentil_90_cr': interval_values['control'][1], 
+                            'v1_percentil_10_cr': interval_values['v1'][0],
+                            'v1_percentil_90_cr': interval_values['v1'][1],
+                            'v2_percentil_10_cr': interval_values['v2'][0],
+                            'v2_percentil_90_cr': interval_values['v2'][1],
+                            'v3_percentil_10_cr': interval_values['v3'][0],
+                            'v3_percentil_90_cr': interval_values['v3'][1],
+                            'v1_prob_to_win_control': loss_list[0],
+                            'v2_prob_to_win_control': loss_list[1],
+                            'v3_prob_to_win_control': loss_list[2]}, index=[0])
 
 
-d = {'variant': variant_name[1:],
- 'Prob. variant winning Control (%)': loss_list}
+  d = {'variant': variant_name[1:],
+  'Prob. variant winning Control (%)': loss_list}
 
 
-table_bayes_cr = pd.DataFrame(data=d)
-st.table(table_bayes_cr)
+  table_bayes_cr = pd.DataFrame(data=d)
+  st.table(table_bayes_cr)
 
 
 
-
-st.download_button(
-    "Download Metrics report as CSV", 
-    csv.to_csv(index=False).encode("utf-8"), 
-    "benchmark-tools.csv",
-    "text/csv",
-    key="download-tools-csv",
-    icon=":material/download:"
-)
+  if st.button("Create report"):
+    st.download_button(
+      "Download Metrics report as CSV", 
+      csv.to_csv(index=False).encode("utf-8"), 
+      "experiment_report.csv",
+      "text/csv",
+      key="download-tools-csv",
+      icon=":material/download:"
+  )
+    st.toast('Your report was created!', icon='😍')
+  
 
 st.subheader('Guidance about choosing the threshold')
 
